b862293b20982e8f43ffa193960f93587b66ab9e
C-D-Lewis <Bonsitm@gmail.com>
2021-03-02 23:31:22 +0000

    work

--- main.py
+++ main.py
@@ -63,9 +63,9 @@ def draw_weather(canvas, image):
   temp_str = f"{weather_data['current_temp']}°C"
   canvas.text((660, 50), temp_str, font = FONT_48, fill = 0)
   temp_high_str = f"{weather_data['temp_high']}°C"
-  canvas.text((660, 80), temp_high_str, font = FONT_28, fill = 0)
+  canvas.text((660, 100), temp_high_str, font = FONT_28, fill = 0)
   temp_low_str = f"{weather_data['temp_low']}°C"
-  canvas.text((700, 80), temp_low_str, font = FONT_28, fill = 0)
+  canvas.text((660, 130), temp_low_str, font = FONT_28, fill = 0)
 
 # Draw things
 def draw():
