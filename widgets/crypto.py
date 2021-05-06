5e408c301413f04e8b4e35dcea1eb2248562a151
C-D-Lewis <Bonsitm@gmail.com>
2021-05-06 12:34:02 +0100

    display mode

--- widgets/crypto.py
+++ widgets/crypto.py
@@ -24,14 +24,48 @@ class CryptoWidget(Widget):
       btc_res = json[0]
       self.btc_data['value'] = round(btc_amount * float(btc_res['price']), 2)
       self.btc_data['change'] = round(btc_amount * float(btc_res['1d']['price_change']), 2)
+      self.btc_data['price_change_pct'] = round(float(btc_res['1d']['price_change_pct']) * 100, 1)
       eth_res = json[1]
       self.eth_data['value'] = round(eth_amount * float(eth_res['price']), 2)
       self.eth_data['change'] = round(eth_amount * float(eth_res['1d']['price_change']), 2)
+      self.eth_data['price_change_pct'] = round(float(eth_res['1d']['price_change_pct']) * 100, 1)
 
       print(f"crypto: {self.btc_data} {self.eth_data}")
       self.unset_error()
     except Exception as err:
       self.set_error(err)
+
+  # Draw the daily change
+  def draw_daily_change(self, image_draw, image):
+    text_x = 95
+    font = fonts.KEEP_CALM_28
+
+    image.paste(images.ICON_BTC, (self.bounds[0], self.bounds[1]))
+    arrow = '+' if self.btc_data['price_change_pct'] > 0 else '-'
+    change_str = f"{arrow}{abs(self.btc_data['price_change_pct'])}%"
+    image_draw.text((text_x, self.bounds[1] + 20), change_str, font = font, fill = 0)
+
+    image.paste(images.ICON_ETH, (self.bounds[0], self.bounds[1] + 74))
+    arrow = '+' if self.eth_data['price_change_pct'] > 0 else '-'
+    change_str = f"{arrow}{abs(self.eth_data['price_change_pct'])}%"
+    image_draw.text((text_x, self.bounds[1] + 94), change_str, font = font, fill = 0)
+
+  # Draw earnings
+  def draw_earnings(self, image_draw, image):
+    text_x = 95
+    font = fonts.KEEP_CALM_28
+
+    image.paste(images.ICON_BTC, (self.bounds[0], self.bounds[1]))
+    arrow = '+' if self.btc_data['change'] > 0 else '-'
+    value_str = f"£{self.btc_data['value']}"
+    change_str = f"{arrow}{abs(self.btc_data['change'])}"
+    image_draw.text((text_x, self.bounds[1] + 20), f"{value_str} ({change_str})", font = font, fill = 0)
+
+    image.paste(images.ICON_ETH, (self.bounds[0], self.bounds[1] + 74))
+    arrow = '+' if self.eth_data['change'] > 0 else '-'
+    value_str = f"£{self.eth_data['value']}"
+    change_str = f"{arrow}{abs(self.eth_data['change'])}"
+    image_draw.text((text_x, self.bounds[1] + 94), f"{value_str} ({change_str})", font = font, fill = 0)
 
   # Draw crypto values
   def draw(self, image_draw, image):
@@ -40,20 +74,13 @@ class CryptoWidget(Widget):
       return
 
     try:
-      text_x = 95
-      font = fonts.KEEP_CALM_28
-
-      image.paste(images.ICON_BTC, (self.bounds[0], self.bounds[1]))
-      arrow = '+' if self.btc_data['change'] > 0 else '-'
-      value_str = f"£{self.btc_data['value']}"
-      change_str = f"{arrow}{abs(self.btc_data['change'])}"
-      image_draw.text((text_x, self.bounds[1] + 20), f"{value_str} ({change_str})", font = font, fill = 0)
-
-      image.paste(images.ICON_ETH, (self.bounds[0], self.bounds[1] + 74))
-      arrow = '+' if self.eth_data['change'] > 0 else '-'
-      value_str = f"£{self.eth_data['value']}"
-      change_str = f"{arrow}{abs(self.eth_data['change'])}"
-      image_draw.text((text_x, self.bounds[1] + 94), f"{value_str} ({change_str})", font = font, fill = 0)
+      DISPLAY_MODE = config.get('CRYPTO_DISPLAY_MODE')  # 'daily_change' or 'earnings'
+      if DISPLAY_MODE == 'daily_change':
+        self.draw_daily_change(image_draw, image)
+      elif DISPLAY_MODE == 'earnings':
+        self.draw_earnings(image_draw, image)
+      else:
+        raise Exception('No crypto mode selected')
     except Exception as err:
       self.set_error(err)
       self.draw_error(image_draw)
